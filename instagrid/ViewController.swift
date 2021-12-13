3e25e5dd4369aff700e3f7eb346ad81bb8adc507
5aitama <alx.sb@icloud.com>
2021-12-13 09:37:59 +0100

    Stable version

--- instagrid/ViewController.swift
+++ instagrid/ViewController.swift
@@ -13,12 +13,19 @@ enum Layout: UInt8 {
     case all  = 0b1111
 }
 
+class CustomTapGesture: UITapGestureRecognizer {
+    var targetView = UIView()
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var configurations: UIStackView!
     @IBOutlet weak var grid: UIStackView!
     
     private var currentLayout = Layout.top
+    private var selectedViewImage: UIView?
+    private var swipeGesture: UISwipeGestureRecognizer!
+    @IBOutlet weak var swipeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,19 +44,47 @@ class ViewController: UIViewController {
         let gestureLayout3 = UITapGestureRecognizer(target: self, action: #selector(handleGestureLayoutAll))
         configurations.subviews[2].addGestureRecognizer(gestureLayout3)
         
+        swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        self.view.addGestureRecognizer(swipeGesture)
+        
         for stack in grid.subviews {
             for view in stack.subviews {
-                let gestureImage = UITapGestureRecognizer(target: self, action: #selector(handleGestureImage))
+                let gestureImage = CustomTapGesture(target: self, action: #selector(handleGestureImage))
+                gestureImage.targetView = view
                 view.addGestureRecognizer(gestureImage)
             }
         }
     }
     
-    @objc func handleGestureImage() {
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        updateSwipeDirection()
+    }
+    
+    func updateSwipeDirection() {
+        if UIDevice.current.orientation.isLandscape {
+            swipeLabel.text = "Swipe left to share"
+            swipeGesture.direction = .left
+        } else {
+            swipeLabel.text = "Swipe up to share"
+            swipeGesture.direction = .up
+        }
+    }
+    
+    @objc func handleSwipe() {
+        let activityView = UIActivityViewController(activityItems: [ "Test" ], applicationActivities: nil)
+        self.present(activityView, animated: true, completion: nil)
+    }
+    
+    @objc func handleGestureImage(sender: CustomTapGesture) {
+        self.selectedViewImage = sender.targetView
+        
         let vc = UIImagePickerController()
-        vc.sourceType = .photoLibrary
-        vc.delegate = self
-        vc.allowsEditing = true
+        
+        vc.sourceType       = .photoLibrary
+        vc.delegate         = self
+        vc.allowsEditing    = true
+        
         present(vc, animated: true)
     }
     
@@ -97,9 +132,15 @@ class ViewController: UIViewController {
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             print(image)
+            // TODO: Change this ...
+            let imageView = (self.selectedViewImage as! UIImageView)
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFill
         }
+        
         picker.dismiss(animated: true, completion: nil)
     }
     
